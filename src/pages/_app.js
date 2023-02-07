f4a467feb011ccdc8fa7e1f4d3e884f4511a5efd
rustmn <allorim@protonmail.com>
2023-02-07 22:53:21 +0300

    i

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
 
+import 'dm-standard/dist/styles/app.scss';
+import 'video.js/dist/video-js.css';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div style = {{
+    width: '100vw',
+    height: '100vh'
+  }}><Component {...pageProps} /></div>
 }
